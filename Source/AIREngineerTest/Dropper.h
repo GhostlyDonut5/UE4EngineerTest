2abdb69176279fd0715773284a444236f35d4d13
GhostlyDonut5 <palitos3@hotmail.com>
2021-05-14 23:28:29 -0700

    Test commit. Added Dropper.

---
+++ Source/AIREngineerTest/Dropper.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RobotInstruction.h"
+#include "Dropper.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AIRENGINEERTEST_API UDropper : public URobotInstruction
+{
+	GENERATED_BODY()
+	
+protected:
+
+public:
+
+	void Test();
+
+};
